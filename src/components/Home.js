c3f288b44058147d4a122a49cc271a2d5f87fbe5
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-15 19:18:29 +0200

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
 import React from 'react'
 // We'll need React Router's own version of the History API
+import { useHistory } from 'react-router-dom'
 
 export default function Home() {
   // 👉 STEP 5 - Build a click handler that will imperatively
   // navigate us to <website base URL>/items-list
+  const history = useHistory() // the react-router replacement for the native History API of the DOM
 
   const routeToShop = () => {
+    // with code
+    // history.pushState(null, null, '/') // we cannot do it like this
 
+    // now we can do logic, for example check 'auth' status
+    if (true) { // authed
+      history.push('/items-list')
+    } else {
+      history.push('/login')
+    }
   }
 
   return (
