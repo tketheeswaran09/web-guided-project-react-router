ff9289adf74dd1e58f8c707f2bca4fda26bb6df3
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-15 20:05:59 +0200

    wip

--- src/components/Item.js
+++ src/components/Item.js
@@ -47,11 +47,11 @@ export default function Item(props) {
 
       {/* 👉 STEP 9 - Here go the Routes for `<current path>/shipping` and `<current path>/description` */}
       {/* These Routes should render <ItemDetails /> */}
-      <Route>
+      <Route path={`${path}/description`}>
         <ItemDetails text={item.description} />
       </Route>
 
-      <Route>
+      <Route path={`${path}/shipping`}>
         <ItemDetails text={item.shipping} />
       </Route>
 
