438901cf7f8706141548cd2e1c827143a9526ecf
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-15 19:05:25 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -36,7 +36,7 @@ export default function App(props) {
       {/* 👉 STEP 4 - Build a Switch with a Route for each of the components imported at the top */}
       {/* with a switch, the first route that matches is rendered only */}
       <Switch>
-        {/* With Route, if URL in Chrome matches the "path" prop we gave the Route, then, Home renders */}
+        {/* With Route, if URL in Chrome matches the "path" prop we gave the Route, then, the nested renders */}
         <Route path="/items-list/:itemId">
           <Item />
         </Route>
