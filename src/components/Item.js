8336e7d8480d3a5b7f976953b05f7cc0bf714f74
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-15 20:04:13 +0200

    wip

--- src/components/Item.js
+++ src/components/Item.js
@@ -47,6 +47,13 @@ export default function Item(props) {
 
       {/* 👉 STEP 9 - Here go the Routes for `<current path>/shipping` and `<current path>/description` */}
       {/* These Routes should render <ItemDetails /> */}
+      <Route>
+        <ItemDetails text={item.description} />
+      </Route>
+
+      <Route>
+        <ItemDetails text={item.shipping} />
+      </Route>
 
       {/* 👉 STEP 10 - Shorten paths and urls with `useRouteMatch` hook */}
     </div>
